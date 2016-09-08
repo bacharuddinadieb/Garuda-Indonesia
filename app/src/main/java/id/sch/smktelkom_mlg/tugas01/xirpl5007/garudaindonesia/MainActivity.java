476f47f9fd54ed71a18d15777f787828463bf6a0
Bacharuddin Adieb Pratama <adieb.co.id@gmail.com>
2016-09-08 12:02:35 +0700

    Booking versi 1.5 Beta Sudah Terdapat Spinner Terdapat Pengolahan Hasil Nama Dan Date

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5007/garudaindonesia/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5007/garudaindonesia/MainActivity.java
@@ -2,12 +2,33 @@ package id.sch.smktelkom_mlg.tugas01.xirpl5007.garudaindonesia;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etnama;
+    EditText etdate;
+    Button bok;
+    TextView tvhasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etnama = (EditText) findViewById(R.id.nama);
+        etdate = (EditText) findViewById(R.id.date);
+        bok = (Button) findViewById(R.id.button);
+        tvhasil = (TextView) findViewById(R.id.hasile);
+        bok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nama1 = etnama.getText().toString();
+                String date1 = etdate.getText().toString();
+                tvhasil.setText("Nama Pemesan : " + nama1 + "\nTanggal Berangkat : " + date1);
+            }
+        });
     }
+
 }
